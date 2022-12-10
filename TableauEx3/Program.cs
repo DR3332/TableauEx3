21ba5eabb68dfba17def7ca5010e1f6f42b72076
DR3332 <105359551+DR3332@users.noreply.github.com>
2022-12-10 08:37:53 +0100

    Modification finale

--- TableauEx3/Program.cs
+++ TableauEx3/Program.cs
@@ -8,29 +8,58 @@ namespace TableauEx3
 {
     class Program
     {
-        int ContrôleEtSaisie(int[] Tableau)
-        {
-            int Température = 0;
 
-            for(int Elements = 0; Elements < Tableau.Length; Elements ++)
+
+        static int ControleErreur(int MauvaiseTemperature)
+        {
+            int Temperature = MauvaiseTemperature;
+            Temperature = 0;
+            bool Correct = false;
+            while (!Correct)
             {
-                bool Correct = false;
-                while(!Correct)
+                try
                 {
-                    try
-                    {
-                        Température = int.Parse(Console.ReadLine());
-                        Correct = true;
-                        Tableau[Elements] = Température;
-                        
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Erreur");
-                    }
+
+                    Temperature = int.Parse(Console.ReadLine());
+                    Correct = true;
+
+
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur");
+                    Correct = false;
                 }
             }
-            return int[] Tableau;
+            return Temperature;
+        }
+        static int ControleTemperature(int Temperature)
+        {
+            int BonneTemperature = 0;
+            while (Temperature < -20 || Temperature > 40)
+            {
+                Console.WriteLine("Erreur");
+                int MauvaiseTemperature = 0;
+                MauvaiseTemperature = Temperature;
+                Temperature = ControleErreur(MauvaiseTemperature);
+            }
+
+            BonneTemperature = Temperature;
+
+            return BonneTemperature;
+        }
+
+  
+        static int[] SaisieFinale(int[] Tableau)
+        {
+            for(int Elements = 0; Elements < Tableau.Length; Elements ++)
+            {
+                
+                Console.WriteLine("Veuillez saisir vos températures entières entre -20 et 40 degrés : ");
+                Console.WriteLine();
+                Tableau[Elements] = ControleTemperature(ControleErreur(0)); ;
+            }
+            return Tableau;
         }
         static void Main(string[] args)
         {
@@ -39,11 +68,23 @@ namespace TableauEx3
             chaque température, il faut afficher sa fréquence (le nombre de fois où cette température a été saisie).
             On ne s’inquiètera pas du problème lié à une saisie erronée au niveau du type d’une variable */
 
-            Console.WriteLine("Veuillez saisir le nombre de températures");
-            int[] Tableau = new int[int.Parse(Console.ReadLine())];
+            Console.WriteLine("Veuillez saisir le nombre de températures : ");
+            int[] TableauInitialisé = new int[int.Parse(Console.ReadLine())];
+            int []TableauFinal = SaisieFinale(TableauInitialisé);
 
-
-            Console.WriteLine();
+            int[] Compteur = new int[TableauFinal.Length]; // Je souhaite faire mon compteur ici
+            for(int J = 0; J < TableauFinal.Length; J ++)
+            {
+                
+            }
+            
+            for(int K = 0; K < TableauFinal.Length; K ++)
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine(TableauFinal[K]);
+            }
+            Console.ReadLine();
             
         }
     }
